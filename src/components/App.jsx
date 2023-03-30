f4fa6b405ab70875fedb5d217d24b002deb62387
Kliuchenko <pasha.kluchenko@gmail.com>
2023-03-30 20:15:51 +0300

    asdfa

---
+++ src/components/App.jsx
@@ -0,0 +1,19 @@
+import { Section } from './Section/Section';
+import { ContactForm } from './ContactForm/ContactForm';
+import { ContactsList } from './ContactList/ContactList';
+import { Filter } from './Filter';
+
+export const App = () => {
+
+    return (
+      <>
+        <Section title="Phonebook">
+          <ContactForm />
+        </Section>
+        <Section title="Contacts">
+          <Filter />
+          <ContactsList />
+        </Section>
+      </>
+    );
+  }
